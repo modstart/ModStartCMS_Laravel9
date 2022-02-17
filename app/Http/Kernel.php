d94dc93f5a0b0087a1255475089017db7c95b201
ModStart <modstart@163.com>
2022-02-17 10:04:35 +0800

    feat: modstart monitor http added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,7 +8,6 @@ class Kernel extends HttpKernel
 {
     
     protected $middleware = [
-        \ModStart\Core\Monitor\ModStartMonitorMiddleware::class,
         \Illuminate\Cookie\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
